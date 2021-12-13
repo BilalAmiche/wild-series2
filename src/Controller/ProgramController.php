c655638bc388c31197e6ffec8a6ff178a21d2fcb
Bilal Amiche <bilal-amiche_student2021@wilder.school>
2021-12-13 11:47:08 +0100

    q5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'program')]
+   /**
+    * @Route("/", name="index")
+    * @return Response
+    */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
          ]);
         }
+
+           /**
+    * @Route("/{id}", requirements={"page"="\d+"}, methods={"GET"}, name="show")
+    * @return Response
+    */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+        }
 }
+
 
